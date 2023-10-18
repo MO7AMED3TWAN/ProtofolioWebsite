de7eba1c891d7c9c1718deb4563bd32a6566a7b8
MoHaMeD <mo7amed3twan@gmail.com>
2023-10-18 16:43:56 +0300

    1

--- js/main.js
+++ js/main.js
@@ -5,3 +5,7 @@ window.addEventListener("scroll", () => {
 	const scrollTop = document.documentElement.scrollTop;
 	el.style.width = `${(scrollTop / height) * 100}%`;
 });
+
+window.addEventListener('beforeunload', function() {
+	window.scrollTo(0, 0);
+});
